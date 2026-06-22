60f23d6e6a62cf0391e133d87a4eda7213ffb850
Arthur Shaw <jashaw2002@gmail.com>
2026-06-22 03:30:55 -0400

    fix(outliers > 6-month non-food exp): refactor code; add messages

--- R/create_issues.R
+++ R/create_issues.R
@@ -733,21 +733,35 @@ create_outlier_issues <- function(
   # 6-month non-food expenditure
   # ----------------------------------------------------------------------------
 
-  issues_non_food_6m <- identify_outliers(
+  non_food_6m_specs <- tibble::tribble(
+    ~ var, ~ by,
+    "hh09c_q03", "non_food_6_months__id",
+  ) |>
+	dplyr::rowwise() |>
+  dplyr::mutate(desc = get_msg("outliers", "non_food_6m", var)) |>
+  dplyr::ungroup()
+
+  issues_non_food_6m <- purrr::pmap(
+    .l = non_food_6m_specs,
+    .f = ~ identify_outliers(
     df_to_check = dfs_filtered$non_food_6_months,
     df_full = dfs_full$non_food_6_months,
-    var = hh09c_q03,
-    by = non_food_6_months__id,
+      var = !!rlang::sym(..1),
+      by = ..2,
     exclude = NULL,
     transform = "log",
     n_mad = 2,
     min_obs = 30,
     bounds = "upper",
     type = 2,
-    # TODO: compose message later
-    desc = "",
-    comment = "",
+      desc = glue::glue(desc),
+      comment = paste(
+        glue::glue(comment_intro),
+        glue::glue(comment_var),
+        comment_body
+      ),
     comment_question = TRUE
+    )
   )
 
   # ----------------------------------------------------------------------------
