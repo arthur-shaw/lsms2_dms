324039eaacdd8f3149a35e4ab1c068c2fae5ed2b
Arthur Shaw <jashaw2002@gmail.com>
2026-07-01 22:59:30 -0400

    feat(check params): put params in a list for easy access

--- R/_setup.R
+++ R/_setup.R
@@ -13,7 +13,35 @@
 # load parameters
 # ------------------------------------------------------------------------------
 
+# load user's config
 source(here::here("_parameters.R"))
+
+# pack into an object to pass around between functions
+params <- list(
+
+	# server details
+	server = server,
+	workspace = workspace,
+	user      = user,
+	password  = password,
+	# questionnaire
+	# household
+	household_qnr_var = household_qnr_var,
+	household_qnr_expr = household_qnr_expr,
+	members_roster_var = members_roster_var,
+	admin1_var = admin1_var,
+	urb_rur_var = urb_rur_var,
+	# community
+	community_qnr_expr = community_qnr_expr,
+	community_qnr_var =community_qnr_var,
+	# validation behavior	
+	suso_statuses_to_reject = suso_statuses_to_reject,
+	issue_codes_to_reject = issue_codes_to_reject,
+	# report period
+	report_start = report_start,
+	report_end = report_end
+	
+)
 
 # ------------------------------------------------------------------------------
 # load functions
